677dd8257d8922fd181181cd865abab864d45d29
Narcodz <naziknassar@gmail.com>
2023-07-09 06:53:17 +0530

    eslint issue

--- app/page.js
+++ app/page.js
@@ -109,7 +109,7 @@ export default function Home() {
             <h1 className='text-3xl py-1 dark:text-white'>Portofolio</h1>
             <p className='text-md py-2 leading-8 text-gray-800 dark:text-white'> 
             {/* {eslint-disable-next-line react/no-unescaped-entities} */}
-            I have designed and developed a portfolio website showcasing my work for a cake client. With a focus on creativity and functionality, I have crafted visually stunning and user-friendly pages that effectively highlight the client's cakes and services.
+            I have designed and developed a portfolio website showcasing my work for a cake client. With a focus on creativity and functionality, I have crafted visually stunning and user-friendly pages that effectively highlight the clients cakes and services.
             </p>
           </div>
           <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap justify-center text-center'>
